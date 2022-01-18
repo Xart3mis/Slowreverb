6b10f435d40e0dbc63756446f46e81f349d115c5
Xart3misX <xart3misx@protonmail.com>
2022-01-18 14:26:14 +0200

    clean up shit

--- main.go
+++ main.go
@@ -7,12 +7,26 @@ import (
 
 func main() {
 	client := lib.Init(60)
-	Result := lib.GetSong("Serpentskirt", "Cocteau Twins", client)
-	fmt.Println(*Result.Filename)
-	file := lib.ModifySpeed(*Result.Filename, 0.9)
+	fmt.Println("Downloading Song:")
+	Result := lib.GetSong("Vixen", "Destroy Boys", client, false)
+	fmt.Println("Done.")
 
+	fmt.Println("Slowing down audio:")
+	file := lib.ModifySpeed(*Result.Filename, 0.75, false)
+	fmt.Println("Done.")
+
+	fmt.Println("\nApplying reverb 1:")
 	file = lib.Reverberize(file, 10, 10, 12, lib.ReverbTypes().Hall.Large_Hall)
-	file = lib.Reverberize(file, 10, 5, 14, lib.ReverbTypes().Hall.Medium_Hall)
+	fmt.Println("Done.")
 
-	lib.Play(file)
+	fmt.Println("\nApplying reverb 2:")
+	file = lib.Reverberize(file, 10, 5, 14, lib.ReverbTypes().Hall.Medium_Hall)
+	fmt.Println("Done.")
+
+	fmt.Println("Playing song:")
+	finished_playing := make(chan bool)
+	go lib.Play(file, finished_playing, false)
+
+	<-finished_playing
+	fmt.Println("Done.")
 }
